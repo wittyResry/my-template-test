2dea4a59703825cd68ed0da17a09817f766d0cba
Qingyu Li <liqingyu.lqy@gmail.com>
2020-03-07 21:33:46 +0800

    designpatterns

---
+++ src/main/java/com/mytest/designpatterns/bridge/RefinedAbstraction.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mytest.designpatterns.bridge;
+
+/**
+ * @author liqingyu
+ * @since 2020/03/07
+ */
+public class RefinedAbstraction extends Abstraction {
+    public RefinedAbstraction(Implementor imp) {
+        super(imp);
+    }
+
+    //抽象角色引用实现角色，或者说抽象角色的部分实现是由实现角色完成的。
+    @Override
+    public void request() {
+        super.request();
+        super.getImp().doAnything();
+    }
+}
