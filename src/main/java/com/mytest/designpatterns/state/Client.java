0b59c07d0a985e59371693972f53217728adeb9b
Qingyu Li <liqingyu.lqy@gmail.com>
2020-03-08 20:02:26 +0800

    state

---
+++ src/main/java/com/mytest/designpatterns/state/Client.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mytest.designpatterns.state;
+
+/**
+ * @author liqingyu
+ * @since 2020/03/08
+ */
+public class Client {
+    public static void main(String[] args) {
+        //定义环境角色
+        Context context = new Context();
+        //初始化状态
+        context.setCurrentState(new ConcreteState1());
+        //行为执行
+        //1 -> 2 ; 1->3 -> 1 -> 2;
+        context.handler();
+    }
+}
---
+++ src/main/java/com/mytest/designpatterns/state/ConcreteState2.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mytest.designpatterns.state;
+
+/**
+ * @author liqingyu
+ * @since 2020/03/08
+ */
+public class ConcreteState2 extends State {
+    @Override
+    public void handle(Context context) {
+        //本状态下要做的事情
+        System.out.println("ConcreteState2 done");
+        //以及本状态如何过渡到其他状态
+        context.setCurrentState(null);
+    }
+}
